0a1c4f316a67c7856fa796471a35631803a0f55c
Nitesh <57097134+Ni-tesh@users.noreply.github.com>
2021-09-18 23:53:12 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -96,3 +96,26 @@ void insert(int a[],int len,int pos, int num)
 		cout<<endl<<a[i];
 	}
 }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a[3][3];
+	int i,j;
+	cin>>s;
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++){
+			cin>>s[i][j];
+		}
+	}
+	
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			cout<<s[i][j]<<" ";
+		}
+		cout<<""<<endl;
+	}
+}
+		
